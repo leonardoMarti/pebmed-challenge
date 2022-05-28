90287eec593ab32e4841281b8113555973f52a07
Leonardo Martinelli <martinelli087@gmail.com>
2022-05-28 09:16:04 -0300

    Add and configure prettier with eslint, fix lint based on rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Head from "next/head";
-import { Header } from "../components/Header";
+import React from 'react';
+import Head from 'next/head';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
