b15046b0893b348be97d9fa6900ff9d887ba5598
Leonardo Martinelli <martinelli087@gmail.com>
2022-05-29 09:09:12 -0300

    Improving radio card style

--- src/components/RadioCard/index.tsx
+++ src/components/RadioCard/index.tsx
@@ -8,12 +8,17 @@ interface RadioCardProps {
 export function RadioCard({ name }: RadioCardProps) {
   return (
     <Container id={name}>
-      <span className="title">Anual | À Vista</span>
-      <div className="priceWrapper">
-        <span className="price">De R$ 514,80 | Por R$ 436,90</span>
-        <div className="discount">-15%</div>
+      <div className="content">
+        <span className="title">Anual | À Vista</span>
+        <div className="priceWrapper">
+          <span className="price">De R$ 514,80 | Por R$ 436,90</span>
+          <div className="discount">-15%</div>
+        </div>
+        <span className="installments">10x de R$ 43,69/mês</span>
       </div>
-      <span className="installments">10x de R$ 43,69/mês</span>
+      <div>
+        <input className="radio" type="radio" name="radio" id="radio1" />
+      </div>
     </Container>
   );
 }
--- src/components/RadioCard/styles.ts
+++ src/components/RadioCard/styles.ts
@@ -3,46 +3,56 @@ import { COLORS } from '../../constants/colors';
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   padding: 1.25rem;
   border: 1px solid ${COLORS.BLUE900};
   border-radius: 15px;
 
-  .title {
-    font-size: 0.875rem;
-    font-weight: 700;
-    line-height: 1.125rem;
-    color: ${COLORS.BLUE900};
-    margin-bottom: 0.25rem;
-  }
-
-  .priceWrapper {
+  .content {
     display: flex;
-    gap: 0.75rem;
-    margin-bottom: 0.25rem;
+    flex-direction: column;
+
+    .title {
+      font-size: 0.875rem;
+      font-weight: 700;
+      line-height: 1.125rem;
+      color: ${COLORS.BLUE900};
+      margin-bottom: 0.25rem;
+    }
+
+    .priceWrapper {
+      display: flex;
+      gap: 0.75rem;
+      margin-bottom: 0.25rem;
+    }
+
+    .price {
+      font-size: 0.75rem;
+      line-height: 1rem;
+      color: ${COLORS.BLUE900};
+    }
+
+    .discount {
+      height: 16px;
+      display: flex;
+      align-items: center;
+      background-color: ${COLORS.ORANGE900};
+      border-radius: 9px;
+      font-size: 0.625rem;
+      line-height: 0.8125rem;
+      color: ${COLORS.WHITE100};
+      padding: 4px 5px 3px 5px;
+    }
+
+    .installments {
+      color: ${COLORS.ORANGE900};
+      font-size: 0.625rem;
+      line-height: 0.8125rem;
+    }
   }
 
-  .price {
-    font-size: 0.75rem;
-    line-height: 1rem;
-    color: ${COLORS.BLUE900};
-  }
-
-  .discount {
-    height: 16px;
-    display: flex;
-    align-items: center;
-    background-color: ${COLORS.ORANGE900};
-    border-radius: 9px;
-    font-size: 0.625rem;
-    line-height: 0.8125rem;
-    color: ${COLORS.WHITE100};
-    padding: 4px 5px 3px 5px;
-  }
-
-  .installments {
-    color: ${COLORS.ORANGE900};
-    font-size: 0.625rem;
-    line-height: 0.8125rem;
+  .radio {
+    margin-left: 1rem;
   }
 `;
